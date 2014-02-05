d1f41c84ca51a18a7ad5d5e43ea43cafbef58e81
DanielTipo <daniel.adamko@gmail.com>
2014-02-05 15:58:39 +0100

    WitBox repo creation

---
+++ modals.js
@@ -0,0 +1,236 @@
+var PinnedBubbleViewport = (function (_super) {
+  __extends(PinnedBubbleViewport, _super);
+  function PinnedBubbleViewport(id) {
+    _super.call(this, id, JST.pinnedviewport(), 'viewport-pinned', 'content', false);
+    this.parameters = { left: 0, top: 0 };
+  }
+  PinnedBubbleViewport.prototype.show = function (callback) {
+    _super.prototype.show.call(this);
+    this.root.css({
+      'left': limit(this.parameters.left, this.root.width(), $(window).width()),
+      'top': limit(this.parameters.top, this.root.height(), $(window).height())
+    });
+    this.frame.css({ opacity: 1 });
+    window.setTimeout(callback, 350);
+  };
+  PinnedBubbleViewport.prototype.hide = function (callback) {
+    _super.prototype.hide.call(this);
+    var self = this;
+    this.frame.css({ opacity: 0 });
+    window.setTimeout(function(){
+      self.root.remove();
+      if(callback) callback();
+    }, 350);
+  };
+  var limit = function(x, a, b) {
+    return (b - x > a) ? x : x - a;
+  };
+  return PinnedBubbleViewport;
+})(WitBox.Viewport);
+
+var BorderedViewport = (function (_super) {
+  __extends(BorderedViewport, _super);
+  function BorderedViewport(id, closeOnClickOverlay) {
+    _super.call(this, id, JST.borderedviewport(), 'viewport-bordered', 'content', true, true, closeOnClickOverlay);
+  }
+  BorderedViewport.prototype.show = function (callback) {
+    _super.prototype.show.call(this);
+    this.frame.css({ opacity: 1 });
+    window.setTimeout(callback, 350);
+  };
+  BorderedViewport.prototype.hide = function (callback) {
+    _super.prototype.hide.call(this);
+    var self = this;
+    this.frame.css({ opacity: 0 });
+    window.setTimeout(function(){
+      self.root.remove();
+      if(callback) callback();
+    }, 350);
+  };
+  return BorderedViewport;
+})(WitBox.Viewport);
+
+var ConfirmModal = (function (_super) {
+  __extends(ConfirmModal, _super);
+  function ConfirmModal(Root) {
+    _super.call(this, Root);
+    this.parameters = { text: '' };
+    this.callbacks = { 'ok': [], 'cancel': [] };
+    this.templateObj = JST.confirmmodal;
+  }
+  ConfirmModal.prototype.initTemplate = function () {
+    _super.prototype.initTemplate.call(this);
+  };
+  return ConfirmModal;
+})(WitBox.Modal);
+
+var AlertModal = (function (_super) {
+  __extends(AlertModal, _super);
+  function AlertModal(Root) {
+    _super.call(this, Root);
+    this.parameters = { text: '' };
+    this.callbacks = { 'ok': [] };
+    this.templateObj = JST.alertmodal;
+  }
+  AlertModal.prototype.initTemplate = function () {
+    _super.prototype.initTemplate.call(this);
+  };
+  return AlertModal;
+})(WitBox.Modal);
+
+var BorderedPinnedViewport = (function (_super) {
+  __extends(BorderedPinnedViewport, _super);
+  function BorderedPinnedViewport(id, closeOnClickOverlay) {
+    _super.call(this, id, JST.borderedpinnedviewport(), 'viewport-bordered-pinned', 'content', true, true, closeOnClickOverlay);
+  }
+  BorderedPinnedViewport.prototype.show = function (callback) {
+    _super.prototype.show.call(this);
+    this.frame.css({ opacity: 1 });
+    window.setTimeout(callback, 350);
+  };
+  BorderedPinnedViewport.prototype.hide = function (callback) {
+    _super.prototype.hide.call(this);
+    var self = this;
+    this.frame.css({ opacity: 0 });
+    window.setTimeout(function(){
+      self.root.remove();
+      if(callback) callback();
+    }, 350);
+  };
+  return BorderedPinnedViewport;
+})(WitBox.Viewport);
+
+var YTModal = (function (_super) {
+  __extends(YTModal, _super);
+  function YTModal(Root) {
+    _super.call(this, Root);
+    this.parameters = { yt: '' };
+    this.templateObj = JST.ytmodal;
+  }
+  YTModal.prototype.initTemplate = function () {
+    _super.prototype.initTemplate.call(this);
+  };
+  return YTModal;
+})(WitBox.Modal);
+
+var GaleryViewport = (function (_super) {
+  __extends(GaleryViewport, _super);
+  var pos = 0;
+  function GaleryViewport(id, closeOnClickOverlay) {
+    _super.call(this, id, JST.galeryviewport(), 'viewport-galery', 'content', true, false, closeOnClickOverlay);
+    this.parameters = { pages: [] };
+  }
+  GaleryViewport.prototype.show = function (callback) {
+    _super.prototype.show.call(this);
+    this.init();
+    this.frame.css({ opacity: 1 });
+    window.setTimeout(callback, 350);
+  };
+  GaleryViewport.prototype.hide = function (callback) {
+    _super.prototype.hide.call(this);
+    var self = this;
+    this.frame.css({ opacity: 0 });
+    window.setTimeout(function(){
+      self.root.remove();
+      if(callback) callback();
+    }, 350);
+  };
+  GaleryViewport.prototype.load = function(p) {
+    var self = this;
+    var content = null;
+    var it = self.parameters.pages[p];
+    self.content.children().css({ 'opacity': 0 });
+    window.setTimeout(function() {
+      self.content.empty();
+      switch(it.type) {
+        case 'img':
+          content = $(JST.galerymodalimg(it));
+          break;
+        case 'yt':
+          content = $(JST.galerymodalyt(it));
+          break;
+      };
+      self.content.append(content);
+      content.css({ 'opacity': 0 });
+      content.load(function() {
+        $.data(content[0], 'width', content.width());
+        $.data(content[0], 'height', content.height());
+        $(window).trigger('resize');
+        window.setTimeout(function() { content.css({ 'opacity': 1 }); }, it.type == 'img' ? 350 : 500);
+      })
+    }, 350);    
+  };
+  GaleryViewport.prototype.refresh_pages = function() {
+    var self = this;
+    self.root.find('.page').removeClass('checked');
+    $.each(self.root.find('.page'), function(i, v) {
+      if($(v).data('id') == pos) {
+        $(v).addClass('checked');
+        return;
+      }
+    });
+  };
+  GaleryViewport.prototype.init = function() {
+    var self = this;
+    pos = 0;
+    self.root.find('.prev-button').click(function(){
+      if(--pos < 0) pos = self.parameters.pages.length - 1;
+      self.refresh_pages();
+      self.load(pos);
+    });
+    self.root.find('.next-button').click(function(){
+      if(++pos > self.parameters.pages.length - 1) pos = 0;
+      self.refresh_pages();
+      self.load(pos);
+    });
+    $(window).resize(function() {
+      var content = self.content.children();
+      var w = $.data(content[0], 'width');
+      var h = $.data(content[0], 'height');
+      if(w > window.innerWidth * 0.2 || h > window.innerHeight * 0.2) {
+        if(w / h > window.innerWidth / window.innerHeight) {
+          content.css({ 'width': window.innerWidth * 0.7, 'height': h / w * window.innerWidth * 0.7 });
+        } else { 
+          content.css({ 'width': w / h * window.innerHeight * 0.7, 'height': window.innerHeight * 0.7 });
+        }
+      }
+      self.content.css({ 
+        'width': content.width() + 'px', 
+        'height': content.height() + 'px', 
+      });      
+      self.content.parent().css({
+        'opacity': 1,
+        'left': window.innerWidth / 2 - content.width() / 2 + 'px',
+        'top': window.innerHeight / 2 - content.height() / 2 + 'px'
+      });
+    });
+    $.each(self.parameters.pages, function(ix, it) {
+      if(it.type == 'img') (new Image).src = it.url;
+      self.root.find('.pages').append($('<span class="page'+(ix == 0 ? ' checked"' : '"')+' data-id="'+ix+'">'+(ix+1)+'</span>'));
+    });
+    self.root.find('.page').click(function() {
+      self.root.find('.page').removeClass('checked');
+      $(this).addClass('checked');
+      pos = $(this).data('id');
+      self.load(pos);
+    });
+    self.load(0);
+  };
+  return GaleryViewport;
+})(WitBox.Viewport);
+
+WitBoxFactory.showConfirm = function(text, okCallback, cancelCallback) {
+  new WitBox.Composite(BorderedViewport, ConfirmModal, { 'ok': { 'click': okCallback }, 'cancel': { 'click': cancelCallback } }, { 'text': text }).show();
+};
+
+WitBoxFactory.showAlert = function(text, okCallback) {
+  new WitBox.Composite(BorderedViewport, AlertModal, { 'ok': { 'click': okCallback } }, { 'text': text }, true).show();
+};
+
+WitBoxFactory.showYT = function(yt) {
+  new WitBox.Composite(BorderedPinnedViewport, YTModal, null, { 'yt': yt }, true).show();
+};
+
+WitBoxFactory.showGalery = function(pages) {
+  new WitBox.Composite(GaleryViewport, null, null, { 'pages': pages }, true).show();
+};
---
+++ witbox.js
@@ -0,0 +1,251 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+
+var WitBox = {
+  KeyValuePair: (function () {
+      function KeyValuePair(name, value) {
+        this.name = name;
+        this.value = value;
+      }
+      return KeyValuePair;
+  })()
+  ,
+  Callback: (function () {
+    function Callback(fnc, params) {
+      var self = this;
+      this.fnc = fnc;
+      this.params = params;
+      this.call = function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        self.fnc(event, self);
+      }
+    }
+      return Callback;
+  })()
+  ,
+  Modal: (function () {
+    function Modal(Root) {
+      this.root = Root;
+      this.parameters = {};
+      this.callbacks = {};
+      this.templateObj = null;
+    }
+    /**
+     * Sets a parameter for the modal template
+     * @param {String} parameterName 
+     * @param {Object} parameterValue
+     */
+    Modal.prototype.setParameter = function (parameterName, parameterValue) {
+      if(typeof parameterName != 'string') throw new Error();
+      this.parameters[parameterName] = parameterValue;
+    };
+    /**
+     * Sets a callback function for an event inside the modal
+     * @param {String} objectName (Class)Name of the object
+     * @param {String} eventFor Event name
+     * @param {Callback} callback Callback object
+     */
+    Modal.prototype.setCallback = function (objectName, eventFor, callback) {
+      if(typeof objectName != 'string') throw new Error();
+      if(typeof eventFor != 'string') throw new Error();
+      if(typeof this.callbacks[objectName] == 'undefined') throw new Error();
+      this.callbacks[objectName][eventFor] = callback;
+    };
+    Modal.prototype.initTemplate = function () {
+      $(this.root).html(this.templateObj(this.parameters));
+    };
+    Modal.prototype.initCallbacks = function () {
+      for(var it in this.callbacks) {
+        if(typeof this.callbacks[it] == 'undefined') continue;
+        for(var c in this.callbacks[it]) {
+          if(typeof this.callbacks[it][c] == 'undefined') continue;
+          $(this.root).find('.' + it).on(c, this.callbacks[it][c].call);
+        }
+      }
+    };
+    Modal.prototype.init = function () {
+      this.initTemplate();
+      this.initCallbacks();
+    };
+    return Modal;
+  })()
+  ,
+  Viewport: (function () {
+    function Viewport(id, html, viewportcls, contentcls, overlayed, centered, overlayexit) {
+      var self = this;
+      this.centered = centered;
+      this.overlayed = overlayed;
+      this.contentClass = contentcls;
+      this.frame = $('<div>');
+      this.frame.attr('id', id);
+      if(overlayed) {
+        this.overlay = $('<div>');
+        this.overlay.addClass('witbox-overlay');
+        this.overlay.appendTo($(document.body));
+        if(overlayexit) {
+          this.overlay.click(function(e){
+            self.hide();
+          });
+        }
+      }
+      this.frame.addClass((centered?'witbox-centered ':'') + 'witbox-' + viewportcls);
+      this.frame.html(html);
+      this.content = this.frame.find('.' + contentcls);
+      this.frame.appendTo($(document.body));
+      this.root = this.frame;
+      this.closeButton = this.frame.find('.close');
+      this.parameters = [];
+      if(centered) {
+        var self = this;
+        $(window).resize(function() {
+          self.frame.css({ 
+            'left': window.innerWidth / 2 - self.frame.width() / 2 + 'px',
+            'top': window.innerHeight / 2 - self.frame.height() / 2 + 'px'
+          });
+        });
+      }
+    };
+    Viewport.prototype.show = function() {
+      if(this.centered) 
+        $(window).trigger('resize');
+    };
+    Viewport.prototype.hide = function() { 
+      $(window).off('resize');
+      if(this.overlayed) 
+        $('.witbox-overlay').remove();
+    };
+    return Viewport;
+  })()
+  ,
+  Composite: (function () {
+    var modalCounter = 0;
+    function Composite(Viewport, Modal, Callbacks, Parameters, CloseOnClickOverlay) {
+      this.viewport = new Viewport("modal-" + (modalCounter++), CloseOnClickOverlay);
+      this.events = { ready: null, open: null, close: null, closed: null };
+      if(Modal) {
+        this.modal = new Modal(this.viewport.content);
+        this.viewport.modal = this.modal;
+        if(Callbacks)
+          for(var it in this.modal.callbacks) {
+            this.modal.callbacks[it] = Callbacks[it] ? Callbacks[it] : [];
+          };
+        if(Parameters)
+          for(var it in this.modal.parameters) {
+            this.modal.parameters[it] = Parameters[it] ? Parameters[it] : [];
+            delete Parameters[it];
+          };
+      }
+      if(Parameters)
+        this.viewport.parameters = Parameters;
+    }
+    Composite.prototype.readyCallback = function (callback) {
+      this.events.ready = callback;
+    };
+    Composite.prototype.openCallback = function (callback) {
+      this.events.open = callback;
+    };
+    Composite.prototype.closeCallback = function (callback) {
+      this.events.close = callback;
+    };
+    Composite.prototype.closedCallback = function (callback) {
+      this.events.closed = callback;
+    };
+    Composite.prototype.show = function () {
+      var self = this;
+      if(self.modal) self.modal.init();
+      self.viewport.frame.find('.close').click(function(){ 
+        self.hide();
+      });
+      self.viewport.show(function() {
+        if(self.events.ready)
+          self.events.ready.fnc(self.events.ready.params);
+      });
+      return this;
+    };
+    Composite.prototype.hide = function () {
+      var self = this;
+      if(self.events.close)
+        self.events.close.fnc(self.events.close.params);
+      self.viewport.hide(function() {
+        if(self.events.closed) 
+         self.events.closed.fnc(self.events.closed.params)
+      });
+      return this;
+    };
+    return Composite;
+  })()
+};
+
+var WitBoxFactory = (function () {
+  var openedAny = null;
+  var openedFor = [];
+  function WitBoxFactory() { }
+  /**
+   * Creates a timed hiding action when leaving the dialog area for at least two seconds
+   * @param {HTMLObject} sender 
+   * @param {WitBox} witbox Witbox object instance
+   */
+  WitBoxFactory.mouseOutAfter2SecondsAction = function (sender, witbox) {
+    var t = 0;
+    var set = function () {
+      t = setTimeout(function(){
+        witbox.hide();
+        WitBoxFactory.isClosed(sender);
+        $(sender).off('mouseenter', unset);
+        $(sender).off('mouseleave', set);
+      }, 2000);
+    };
+    var unset = function () {
+      clearTimeout(t);
+    };
+    openedAny = witbox;
+    $(witbox.viewport.root).mouseleave(set);
+    $(witbox.viewport.root).mouseenter(unset);
+    $(sender).mouseenter(unset);
+    $(sender).mouseleave(set);
+  };
+  WitBoxFactory.closeButtonAction = function (sender, witbox) {
+    witbox.viewport.closeButton.click(function(){
+      WitBoxFactory.isClosed(sender);
+    });
+  };
+  /**
+   * @returns True if a witbox modal already opened for @sender
+   * @param {HTMLObject} sender
+   */
+  WitBoxFactory.isOpened = function(sender) {
+    var result = openedFor.indexOf(sender);
+    if(result > -1) return true;
+    openedFor.push(sender);
+    return false;
+  };
+  /**
+   * @returns True if no witbox modal opened for @sender
+   * @param {HTMLObject} sender
+   */
+  WitBoxFactory.isClosed = function(sender) {
+    var result = openedFor.indexOf(sender);
+    if(result == -1) return true;
+    delete openedFor[openedFor.indexOf(sender)];
+    return false;
+  };
+  /**
+   * Hides the opened modals
+   */
+  WitBoxFactory.hideAll = function() {
+    if(openedAny) {
+      openedAny.hide();
+      openedAny = null;
+    }
+  }
+  return WitBoxFactory;
+})();
+
+
+
+
